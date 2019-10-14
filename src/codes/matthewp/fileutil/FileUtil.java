da69116e73de2fdf5564da8b7b30a8be67a389a2
xXXIMMMATTHEWXXx <mattparksjr@gmail.com>
2019-10-14 12:17:35 -0400

    Rewrite command handling

--- src/codes/matthewp/fileutil/FileUtil.java
+++ src/codes/matthewp/fileutil/FileUtil.java
@@ -8,62 +8,56 @@ import java.util.Scanner;
 
 public class FileUtil {
 
-    private static Map<String, ICommand> possibleCommands = new HashMap<String, ICommand>();
+    private static Map<String, ICommand> possibleCommands = new HashMap<>();
 
     public static void main(String[] args) {
         // Register commands
-        possibleCommands.put(new ReadDictionary().getCommand(), new ReadDictionary());
-        possibleCommands.put(new ListContentsByExt().getCommand(), new ListContentsByExt());
-        possibleCommands.put(new CheckReadWrite().getCommand(), new CheckReadWrite());
-        possibleCommands.put(new IsValidDir().getCommand(), new IsValidDir());
+        registerCommands(new ReadDictionary(), new ListContentsByExt(), new CheckReadWrite(), new IsValidDir(), new GetFileSize(), new FindLongestWord());
         // possibleCommands.add("-compareLex");
         // possibleCommands.add("-lastModificationTime");
         // possibleCommands.add("-listFileContents");
-        possibleCommands.put(new GetFileSize().getCommand(), new GetFileSize());
         // possibleCommands.add("-contentsToByteArray");
-		possibleCommands.put(new FindLongestWord().getCommand(), new FindLongestWord());
         // possibleCommands.add("-readLines");
         // possibleCommands.add("-append");
         // possibleCommands.add("-readStoreToArray");
         // possibleCommands.add("-readFile");
-		doArgProcess(args);
+        doArgProcess(args);
     }
 
-	private static void doArgProcess(String[] args) {
-		if (args.length > 0) {
-			if (args[0].startsWith("-")) {
-				for (int i = 0; i < possibleCommands.size(); i++) {
-					String[] strippedArgs = new String[args.length];
-					for (int j = 0; j < args.length; j++) {
-						if (j != 0) {
-							strippedArgs[j - 1] = args[j];
-						}
-					}
-					if (possibleCommands.get(args[0]) != null) {
-						possibleCommands.get(args[0]).processCommand(strippedArgs);
-					} else {
-						printHelp();
-					}
-				}
-			} else {
-				printHelp();
-			}
-		} else {
-			printHelp();
-		}
-	}
+    private static void doArgProcess(String[] args) {
+        if (args.length > 0) {
+            if (args[0].startsWith("-")) {
+                for (int i = 0; i < possibleCommands.size(); i++) {
+                    String[] strippedArgs = new String[args.length];
+                    for (int j = 0; j < args.length; j++) {
+                        if (j != 0) {
+                            strippedArgs[j - 1] = args[j];
+                        }
+                    }
+                    if (possibleCommands.get(args[0]) != null) {
+                        possibleCommands.get(args[0]).processCommand(strippedArgs);
+                    } else {
+                        printHelp();
+                    }
+                }
+            } else {
+                printHelp();
+            }
+        } else {
+            printHelp();
+        }
+    }
 
     private static void printHelp() {
         System.out.println("FileUtil help ->");
         System.out.println("FileUtil may be run with the following commands:");
-        for(String s : possibleCommands.keySet()) {
-			System.out.println("    " + possibleCommands.get(s).getUsage());
-		}
+        for (String s : possibleCommands.keySet()) {
+            System.out.println("    " + possibleCommands.get(s).getUsage());
+        }
 //        System.out.println("    -compareLex [file] [file]");
 //        System.out.println("    -lastModificationTime [file]");
 //        System.out.println("    -listFileContents [dictionary]");
 //        System.out.println("    -contentsToByteArray [file]");
-//        System.out.println("    -locateLongestWord [file]");
 //        System.out.println("    -readLines [file]");
 //        System.out.println("    -append [file] [textToBeAppended]");
 //        System.out.println("    -readStoreToArray [file] [echo?t/f]");
@@ -72,9 +66,19 @@ public class FileUtil {
         while (true) {
             String input = userInput.nextLine();
             if (!input.isEmpty()) {
-            	String[] args = input.split(" ");
-               	doArgProcess(args);
+                String[] args = input.split(" ");
+                doArgProcess(args);
             }
         }
+    }
+
+    private static void registerCommands(ICommand... commands) {
+        for (ICommand command : commands) {
+            registerCommand(command);
+        }
+    }
+
+    private static void registerCommand(ICommand command) {
+        possibleCommands.put(command.getCommand(), command);
     }
 }
